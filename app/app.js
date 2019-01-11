2e8f4bb7861b24350b2b3276cd40ab8afc162aca
wangchi <wangchi01@bianfeng.com>
2019-01-11 10:21:23 +0800

    add

--- app/app.js
+++ app/app.js
@@ -1,32 +1,24 @@
-//引入http模块
-const http = require('http');
-//引入fs模块
+'use strict';
+const express = require('express');
+const path = require('path');
+const app = express();
 const fs = require('fs');
-//引入url模块
-const url = require('url');
-//开启本地http服务，监听相应端口号
-http.createServer(function(req,res){
-  //获取requset信息中的host地址
-  let hostname = req.headers.host;
-  //获取pathname
-  let pathname = url.parse(req.url).pathname;
-  //判断是否为域名地址（简单路由）
-  if(pathname === '/'){
-    readFileAndResponse('../index.html',res);
-  }
-}).listen(80);
 
-//读取文件并返回response
-function readFileAndResponse(pathname,response){
-  //判断文件是否存在
-  fs.readFile(pathname.substr(1),'',function(err,data){
-    //文件不存在或读取错误返回404，并打印page not found
-    if(err){
-      response.writeHead(404);
-      response.end('page not found');
+console.log(path.resolve('./public/src/index.html'));
+
+// 路由
+app.get('/', (req, res) => {
+  fs.readFile(path.resolve('./public/src/index.html'), (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end('page not found')
     } else {
-      //读取成功返回相应页面信息
-      response.end(data);
+      res.end(data)
     }
-  });
-}
+  })
+});
+
+// 端口
+app.listen(8081, () => {
+  console.log('express is listening')
+});
